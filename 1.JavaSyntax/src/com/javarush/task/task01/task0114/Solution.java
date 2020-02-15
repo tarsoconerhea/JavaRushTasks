e58642917071775c6f12385cb5d30162e484272d
Daniel <freddy16.91@mail.ru>
2020-02-15 11:46:09 +0300

    //

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0114/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task01.task0114;
+
+/* 
+JavaRush. Learn once - use anywhere
+Напиши программу, которая выводит на экран надпись: "JavaRush. Learn once - use anywhere" 10 раз.
+Требования:
+1. Программа должна выводить текст.
+2. Текст должен начинаться с "JavaRush".
+3. Текст должен заканчиваться на "use anywhere".
+4. Текст должен состоять из 10 строк.
+5. Выводимый текст должен соответствовать заданию.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "JavaRush. Learn once - use anywhere";
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
