393d3d0c74ed1ffc11d47dbe6728917223077367
Daniel <freddy16.91@mail.ru>
2020-03-07 12:36:47 +0300

    //

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("У лукоморья дуб зелёный;");
+        System.out.println("Златая цепь на дубе том:");
+        System.out.println("И днём и ночью кот учёный");
+        System.out.println("Всё ходит по цепи кругом;");
+        //напишите тут ваш код
+    }
+}
